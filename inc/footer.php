773bc6ecfad243e6b06a80bf3142cf61c9f140ae
guillermo <reyesmendoguillermoraul@gmail.com>
2022-04-15 00:11:04 -0500

    Version 2022-1

---
+++ inc/footer.php
@@ -0,0 +1,44 @@
+<div class="ResForm"></div>
+<div class="ResbeforeSend"></div>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <h4 class="text-footer">Contáctanos</h4>
+                <a href="#" target="_blank">
+                    <i class="fa fa-facebook" aria-hidden="true">&nbsp; Facebook </i> 
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-twitter" aria-hidden="true">&nbsp; Twitter </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-youtube-play" aria-hidden="true">&nbsp; YouTube </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-instagram" aria-hidden="true">&nbsp; Instagram </i>
+                </a><br>
+                <!--a href="#" target="_blank">
+                    <i class="fa fa-map-marker" aria-hidden="true">&nbsp; Encuentranos </i>
+                </a-->
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer">Porque elegirnos</h4>
+                <p> 
+                    Somo una empresa en crecimiento, contando con los </br>
+                    mejores precios y ofertas del mercado. Distribuimos</br>
+                    las mejores marcar y brindamos el mejor soporte que</br>
+                    para cada unos de nuestros productos.
+                </p>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer" >Ubicanos en:</h4>
+                <p style="color: #FFF">San Juan de Lurigancho.</br>
+                <p style="color: #FFF">Lima - Perú.</br>                
+                <p style="font-size:15px" class="fa fa-mobile">  (+51) 986 322 205</p> <!--a href="#" target="_blank" style="color: #5bc0de">envianos_un_mensaje</a--></br>
+                E-mail: <a href="#" target="_blank" style="color: #5bc0de"> &nbsp; ventas@groupwsidesign.com</a>
+            </div>
+        </div>
+    </div>
+    <br><br><br>
+    <h5 class="text-center tittles-pages-logo text-footer">STORETECH &copy; 2022</h5>
+</footer>
